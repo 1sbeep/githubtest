e26f7b7bfb98edd8c6b3d87b892290c112d92c60
1sbeep <shobidhansivasothy@gmail.com>
2025-04-10 14:50:33 +0200

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ package githubtest;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
     }
 
     //what is going on?
